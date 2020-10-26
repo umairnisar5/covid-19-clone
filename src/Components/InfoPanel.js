99e3831139a9baefa275fdc032a5334d1b9f1bc3
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-10-27 02:02:09 +0500

    add component

---
+++ src/Components/InfoPanel.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // flexGrow: 1,
+    maxWidth :1000,
+    margin : '0 auto',
+    marginTop: 50
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function InfoPanel() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>Grid 1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>Grid 2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>Grid 3</Paper>
+        </Grid> 
+      </Grid>
+    </div>
+  );
+}
